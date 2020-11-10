e6a85868923959992f79c151efb540ec19e86137
Andrej Sebestyen <andrej.sebestyen@smeonline.sk>
2020-11-10 09:48:34 +0000

    fix store name issue @ stores_proc.sql

--- models/admin/stores_proc.sql
+++ models/admin/stores_proc.sql
@@ -15,8 +15,8 @@ account,
 platform,
 time_of_entry,
 first_value(time_of_entry) OVER (PARTITION BY store ORDER BY time_of_entry DESC) lv
-FROM `{{ target.project }}.dbt_buyer_segmentation.data_feeds` 
-where store_name != ''
+FROM `{{ target.project }}.agency_data_pipeline.data_feeds` 
+where name != ''
 
 ) 
 
